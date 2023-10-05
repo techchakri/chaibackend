5b79eeb428c725d5e53760cb968a7589d36a6585
Chakravarth <chakrijallu7@gmail.com>
2023-10-05 05:54:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express()
+const port = process.env.PORT || 4001;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+});
+
+app.get("/twitter", (req, res) => {
+    res.send("chakravarthidotcom")
+})
+
+app.get("/login", (req, res) => {
+    res.send('<h1>please login at chai aur code</h1>');
+})
+
+app.get("/youtube", (req, res) => {
+    res.send("<h2>Chai aur code</h2>")
+})
+
+app.listen(port, () => {
+    console.log(`Node Server is Running on port ${port}`);
+});
