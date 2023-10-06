c272bbd07f426326a4f13d29bc43d9eaca0418f0
Chakravarth <chakrijallu7@gmail.com>
2023-10-06 11:34:46 +0530

    added new api register

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ app.get("/", (req, res) => {
 
 app.get("/twitter", (req, res) => {
     res.send("chakravarthidotcom")
-})
+});
 
 app.get("/login", (req, res) => {
     res.send('<h1>please login at chai aur code</h1>');
-})
+});
 
 app.get("/youtube", (req, res) => {
     res.send("<h2>Chai aur code</h2>")
-})
+});
+
+app.get("/register", (req, res) => {
+    res.send("<h2>please register at chai aur code</h2>");
+});
 
 app.listen(port, () => {
     console.log(`Node Server is Running on port ${port}`);
